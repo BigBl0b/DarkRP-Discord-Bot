103112031417f32afd2e2c0ca614784642a6b757
Galaxy Man <40147633+BigBl0b@users.noreply.github.com>
2022-03-04 14:00:07 +0000

    --

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-const Discord = require('discord.js')
-const config = require('./config.json')
+const Discord = require("discord.js")
+const config = require("./config.json")
 const bot = new Discord.Client();
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 const RegisterCommands = require('./registery/registerCommands');
 const registerCmds = new RegisterCommands('./commands', ['general', 'misc', 'moderation'], client);
-const Gamedig = require('gamedig');
 registerCmds.init();
 
-client.on('ready', () => {
-  console.log('General Commands Ready');
-  console.log('Miscellaneous Commands Ready');
-  console.log('Moderation Commands Ready');
-  console.log('Bot is online!');
+client.on("ready", () => {
+  console.log("General Commands Ready");
+  console.log("Miscellaneous Commands Ready");
+  console.log("Moderation Commands Ready");
+  console.log("Bot is online!");
 
-  setInterval(async () => {
-    try {
-      state = await Gamedig.query({
-        type: 'garrysmod',
-        host: '54.37.244.52',
-        port: '27015',
-      })
+  const Gamedig = require('gamedig');
+  Gamedig.query({
+      type: 'garrysmod',
+      host: '54.37.244.52', // This needs to be a string
+      port: '27015' // This needs to be a number & is optional, unless you're not using the default port for that gameserver type
+  }).then((state) => {
+      console.log(state); // You can use this output to get the player count.
+      let players = state.players.length;
+      let maxPlayers = state.maxplayers;
 
-
-      const {
-        maxplayers : MaxPlayers,
-        players: { length: Players}
-      } = state;
-
-      client.user.setActivity(Players+ ' / '+MaxPlayers +' Players');
-    } catch (e) {
-      console.log(e);
-    }
-  }, 6000);
+      client.user.setActivity(players + " / " + maxPlayers);
+  }).catch((error) => {
+      console.log("Server is offline");
+  });
 });
+// client.user.setActivity(Players+ ' / '+MaxPlayers +' Players');
 
 
-client.on('message', message => {
-  const args = message.content.slice(config.prefix.length).split(' ');
+client.on("message", message => {
+  const args = message.content.slice(config.prefix.length).split(" ");
   const command = args.shift();
   const commandFile = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
